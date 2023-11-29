53caf1f57e6f4c2d9b7ad441f48be28854596886
Evgeny Zemtsov <eze@resoptima.com>
2023-11-29 18:44:31 +0100

    make tab bar look nice

--- config/custom.el
+++ config/custom.el
@@ -4,10 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+   '("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
+ '(js-indent-level 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(tab-bar ((t (:background "#282a2e" :foreground "#969896" :height 100))))
+ '(tab-bar- ((t (:background "#373b41" :foreground "#b294bb" :inverse-video nil :box nil))))
+ '(tab-bar-tab ((t (:background "#373b41" :foreground "#b294bb" :inverse-video nil :box nil)))))
